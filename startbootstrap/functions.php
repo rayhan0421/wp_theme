bdea623a7d17329c360d4841ae276482aee9efc9
Rayhan Uddin <rayhan0421@gmail.com>
2017-01-08 23:12:52 +0600

    theme basic header footer and css/js added

---
+++ startbootstrap/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+
+function start_bootstrap_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/vendor/bootstrap/css/bootstrap.min.css', array(), '3.1', 'all');
+    wp_enqueue_style( 'fontawsome', get_template_directory_uri() . '/vendor/font-awesome/css/font-awesome.min.css', array(), '3.1', 'all');
+    wp_enqueue_style( 'simple', get_template_directory_uri() . '/vendor/simple-line-icons/css/simple-line-icons.css', array(), '3.1', 'all');
+    wp_enqueue_style( 'device-mock', get_template_directory_uri() . '/vendor/device-mockups/device-mockups.min.css', array(), '3.1', 'all');
+    wp_enqueue_style( 'nav', get_template_directory_uri() . '/css/new-age.min.css', array(), '3.1', 'all');
+    wp_enqueue_style( 'lato', 'https://fonts.googleapis.com/css?family=Lato', array(), '3.1', 'all');
+    wp_enqueue_style( 'cata', 'https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900', array(), '3.1', 'all');
+    wp_enqueue_style( 'multi', 'https://fonts.googleapis.com/css?family=Muli', array(), '3.1', 'all');
+
+    wp_enqueue_script( 'script', 'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js', array (  ), 1.0, false);
+
+    wp_enqueue_script( 'respond', 'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js', array (  ), 1.1, false);
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
+
+    wp_enqueue_script( 'script', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js', array (  ), 1.0, true);
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/new-age.min.js', array ( 'jquery' ), 1.1, true);
+}
+add_action( 'wp_enqueue_scripts', 'start_bootstrap_scripts' );
---
+++ startbootstrap/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>New Age - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+   <?php wp_head(); ?>
+
+
+
+
+
+</head>
+
+<body id="page-top" <?php body_class(); ?>>
+
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a class="page-scroll" href="#download">Download</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#features">Features</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
